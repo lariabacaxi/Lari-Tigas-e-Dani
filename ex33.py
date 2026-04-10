e3daba9451c57a2dd8f42b055052ab68c7aa0dec
lariabacaxi <larissa.nagel.alves@escola.pr.gov.br>
2026-04-10 09:49:08 -0300

    Create ex33.py

---
+++ ex33.py
@@ -0,0 +1,14 @@
+# Exercício 33 
+# Somar números até digiatar 0
+
+soma = 0
+
+while True: 
+  numero = int(input("Digite um número (0 para sair): "))
+  
+  if numero == 0:
+    break
+
+  soma = soma + numero
+
+print("Soma:", soma)
