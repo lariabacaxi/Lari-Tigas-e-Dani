eeed09c8b711241ee58dddf2ef69bf8379d5b082
lariabacaxi <larissa.nagel.alves@escola.pr.gov.br>
2026-04-10 09:52:27 -0300

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,14 @@
+# Exercício 34
+# Contar números até digitar 0
+
+soma = 0
+
+while True:
+  numero = int(input("Digite um número (0 para sair): "))
+
+  if numero == 0:
+     break
+
+  soma = soma + numero
+
+print("quantidade", contador)
