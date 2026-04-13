642fadab21df9de5e67baa25a944e3055599f226
Julia Artigas <julia.artigas.castro@escola.pr.gov.br>
2026-04-13 09:26:18 -0300

    Create ex39.py

---
+++ ex39.py
@@ -0,0 +1,11 @@
+# Exercicio 39 
+# Função simples
+
+def mensagem():
+    print("Olá")
+    nome = input("Qual é o seu nome? ")
+    print(f"Bem vindo {nome}")
+
+mensagem()
+mensagem()
+mensagem()
